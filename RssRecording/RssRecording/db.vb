48d2a83ba8940842af8fb0ea36c7ef9eca31b408
Drewry Pope <dezren39@users.noreply.github.com>
2017-03-09 08:27:01 -0600

    Delete db.vb

--- RssRecording/RssRecording/db.vb
+++
@@ -1,7 +0,0 @@
-﻿Imports System.Data.SqlClient
-Public Class db
-    Public Function connect() As SqlConnection
-        Return New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\dpope3\Desktop\RssRecording\RssRecording\SchoolRssRecording.mdf;Integrated Security=True")
-    End Function
-
-End Class
