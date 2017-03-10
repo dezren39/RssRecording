a1813fa7113ae43cb3ece3a14992b8b7c58a2f0f
Drewry Pope <dezren39@users.noreply.github.com>
2017-03-09 18:35:25 -0600

    Add files via upload

---
+++ RssRecording-master/RssRecording-master/RssRecording/RssRecording/Rss.vb
@@ -0,0 +1,49 @@
+﻿Public Class Rss
+    Private idVal As Integer
+    Private lastXmlValue As XDocument
+    Private titleValue As String
+    Private urlValue As String
+
+    Sub New()
+    End Sub
+
+    Sub New(ByVal urlValue As String)
+        Me.Url = urlValue
+    End Sub
+
+    Public Property Id() As Integer
+        Get
+            Return idVal
+        End Get
+        Set(ByVal value As Integer)
+            idVal = value
+        End Set
+    End Property
+
+    Public Property LastXML() As XDocument
+        Get
+            Return lastXmlValue
+        End Get
+        Set(ByVal value As XDocument)
+            lastXmlValue = value
+        End Set
+    End Property
+
+    Public Property Title() As String
+        Get
+            Return titleValue
+        End Get
+        Set(ByVal value As String)
+            titleValue = value
+        End Set
+    End Property
+
+    Public Property Url() As String
+        Get
+            Return urlValue
+        End Get
+        Set(ByVal value As String)
+            urlValue = value
+        End Set
+    End Property
+End Class
